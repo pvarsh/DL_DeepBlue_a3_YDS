94ebace51bf1e072c0bf11c3b727dbf831c8828e
Peter Varshavsky <pvarsh@gmail.com>
2015-03-20 00:56:55 -0400

    Now :forward() returns tensor of correct length

--- pooling_lua/pooling_test_net.lua
+++ pooling_lua/pooling_test_net.lua
@@ -18,21 +18,23 @@ function TemporalLogExpPooling:updateOutput(input)
    -----------------------------------------------
    
    local input_length = input:size()[1]
-
+   local output_length = math.floor((input_length - self.kW)/self.dW + 1)
+   print("Output length: ", output_length)
+   self.output = torch.DoubleTensor(output_length)
    local y = torch.DoubleTensor(input:size())
 
    y:mul(input, self.beta) -- multiplication and exponentiation
    y:exp()                 -- only is done once
    
    local pos = 1
-   local cumsum = 0
+   local count = 1
    while pos + (self.kW - 1) <= input_length do
       print("Position", pos)
-      cumsum = cumsum + y[{ {pos,(pos + self.kW - 1)} }]:sum()
+      self.output[count] = y[{ {pos,(pos + self.kW - 1)} }]:sum()
       pos = pos + self.dW
+      count = count + 1
    end
-
-   self.output = math.log(cumsum / self.kW) / self.beta
+   ((self.output:mul(1/self.kW)):log()):mul(1/self.beta)
 
    return self.output
 end
@@ -41,6 +43,7 @@ function TemporalLogExpPooling:updateGradInput(input, gradOutput)
    -----------------------------------------------
    -- your code here
    -----------------------------------------------
+   print("SIIIiZE: ", self.output:size())
    return self.gradInput
 end
 
@@ -60,6 +63,7 @@ require 'nn';
 
 ninputs = 10
 
+
 x = torch.rand(ninputs,1)
 print(x)
 
@@ -68,6 +72,7 @@ print(x)
 -- model:add(TemporalLogExpPooling(3,1,.5))
 
 -- If TemporalLogExpPooling is used without nn.Sequential container, it seems to work
-model = nn.TemporalLogExpPooling(5, 4, .5)
+model = nn.TemporalLogExpPooling(3, 2, .5)
 
 print(model:forward(x))
+-- print(model:backward())
