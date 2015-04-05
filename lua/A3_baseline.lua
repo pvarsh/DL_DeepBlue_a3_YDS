1b0096c92c0bfd8b5b09be63dcd4a8db2090adbc
Peter Varshavsky <pvarsh@gmail.com>
2015-04-05 11:48:32 -0400

    Removes old code. Adds status print statement

--- lua/A3_baseline.lua
+++ lua/A3_baseline.lua
@@ -181,6 +181,7 @@ function main()
 
     criterion = nn.ClassNLLCriterion()
    
+    print("Training...")
     train_model(model, criterion, training_data, training_labels, test_data, test_labels, opt)
     local results = test_model(model, test_data, test_labels)
     print(results)
