f69b290c8f5b56195f7f3dc7079fa180e657bcd8
Peter Varshavsky <pvarsh@gmail.com>
2015-04-06 18:25:38 -0400

    Sets hidden layer to 2*input_size

--- lua/A3_baseline.lua
+++ lua/A3_baseline.lua
@@ -151,7 +151,7 @@ function main(opt)
     -- if you decide to just adapt the baseline code for part 2, you'll probably want to make this linear and remove pooling
     -- model:add(nn.TemporalConvolution(1, 20, 10, 1))
     model:add(nn.Reshape(opt.minibatchSize*opt.inputDim))
-    model:add(nn.Linear(opt.minibatchSize*opt.inputDim, 20*(opt.inputDim-11)))
+    model:add(nn.Linear(opt.minibatchSize*opt.inputDim, opt.minibatchSize*opt.inputDim*2))
     model:add(nn.ReLU())
     model:add(nn.Dropout(0.5)) 
     -- if opt.pooling == 'max' then
@@ -163,7 +163,7 @@ function main(opt)
     -- end
     
     -- model:add(nn.Reshape(20*(opt.inputDim-11), true))
-    model:add(nn.Linear(20*(opt.inputDim-11), 5))
+    model:add(nn.Linear(opt.minibatchSize*opt.inputDim*2, 5))
     model:add(nn.LogSoftMax())
 
     criterion = nn.ClassNLLCriterion()
