94c449f8b9dc17c4b6b09ee710c6b633d97f58b9
Peter Varshavsky <pvarsh@gmail.com>
2015-04-08 13:40:19 -0400

    Adds conv baseline model

---
+++ lua/models.lua
@@ -0,0 +1,36 @@
+function linear_baseline(opt)    
+    model = nn.Sequential()
+   
+    model:add(nn.Reshape(opt.minibatchSize*opt.inputDim))
+    model:add(nn.Linear(opt.minibatchSize*opt.inputDim, opt.minibatchSize*opt.inputDim*2))
+    model:add(nn.ReLU())
+    model:add(nn.Dropout(0.5)) 
+    model:add(nn.Linear(opt.minibatchSize*opt.inputDim*2, 5))
+    model:add(nn.LogSoftMax())
+
+    criterion = nn.ClassNLLCriterion()
+
+    return model, criterion
+end
+
+function conv_baseline(opt)
+    model = nn.Sequential()
+    model:add(nn.TemporalConvolution(1, 20, 10, 1))
+    
+    if opt.pooling == 'max' then
+        model:add(nn.TemporalMaxPooling(3, 1))
+    elseif opt.pooling == 'logexp' then
+        model:add(nn.TemporalLogExpPooling(3, 1, opt.beta))
+    else
+        error("opt.pooling must be 'max' or 'logexp'")
+    end
+    
+    -- subtracting 11 may break the code if step or window size is changed
+    model:add(nn.Reshape(20*(opt.inputDim-11), true))
+    model:add(nn.Linear(20*(opt.inputDim-11), 5))
+    model:add(nn.LogSoftMax())
+
+    criterion = nn.ClassNLLCriterion()
+
+    return model, criterion
+end
