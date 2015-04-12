a617265a0779c3e9c370126b7317483aaa8b4d62
EmilRex <emil.christensen@enigma.io>
2015-04-12 17:02:18 -0400

    Began assembling submission files

---
+++ DEEP BLUE.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#PBS -l nodes=1:ppn=1
+#PBS -l mem=32GB
+#PBS -l walltime=2:00:00
+#PBS -N DEEP BLUE
+ 
+module purge
+module load torch-deps/7
+
+# Define path to torch
+alias th=/home/erc399/torch/install/bin/th
+
+# Make team directory
+cd $HOME
+mkdir DEEP\ BLUE
+cd DEEP_BLUE
+
+# Define download URL
+url=http://cims.nyu.edu/~erc399/DL_DeepBlue_a3_YDS/submission/
+
+# Download files
+# Get skeleton
+wget ${url}DEEP\ BLUE_A3_skeleton.lua
+
+# Get baseline
+wget ${url}DEEP\ BLUE_A3_baseline.lua
+
+# Get model
+wget ${url}DEEP\ BLUE_model.sh
+
+# Get paper
+wget ${url}DEEP\ BLUE_model.pdf
