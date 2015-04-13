84bb351f47594b950939e16c69c2f194a2f3c9c2
Priyank Bhatia <pb1672@nyu.edu>
2015-04-12 21:26:07 -0400

    no error , epoch 1 too slow

--- lua/crepe.lua
+++ lua/crepe.lua
@@ -103,7 +103,7 @@ function train_model(model, criterion, training_data, training_labels, opt)
 		-- create mini batch
 		if t + opt.batchSize-1 <= training_data:size(1) then
 			-- xx = opt.batchSize
-			print("in if stmt")
+--			print("in if stmt")
 			inputs[{}] = training_data[{ {t,t+opt.batchSize-1},{},{} }]
 			targets[{}] = training_labels[{ {t,t+opt.batchSize-1} }]
 		
@@ -123,21 +123,29 @@ function train_model(model, criterion, training_data, training_labels, opt)
                        -- evaluate function for complete mini batch
                        for i = 1,inputs:size(1) do
                           -- estimate f
-                          local output = model:forward(inputs[i])
-                          local err = criterion:forward(output, targets[i])
+                          local output = model:forward(inputs[{ {i} }])
+--                          print("Output size")
+--                          print(output:size())
+                          local err = criterion:forward(output, targets[{ {i} }])
                           f = f + err
 
                           -- estimate df/dW
-                          local df_do = criterion:backward(output, targets[i])
-                          model:backward(inputs[i], df_do)
+                          local df_do = criterion:backward(output, targets[{ {i} }])
+                          model:backward(inputs[{ {i} }], df_do)
+--                          print("Target")
+--                          print(targets[{ {i} }])
 
                           -- update confusion
-                          confusion:add(output, targets[i])
+--                          for k=1,opt.batchSize do
+--                    			confusion:add(model.output[k], targets[{ {k} }])
+--                		  end
+
+--                          confusion:add(output, targets[{ {i} }])
                        end
 
                        -- normalize gradients and f(X)
-                       gradParameters:div(#inputs)
-                       f = f/#inputs
+--                       gradParameters:div(inputs:len())
+--                       f = f/#inputs
 
                        -- return f and df/dX
                        return f,gradParameters
@@ -276,19 +284,20 @@ function main()
 
     -- build model *****************************************************************************
     model = nn.Sequential()
+    print("after seq")
     -- first layer (#inputDim x 204)
-    model:add(nn.TemporalConvolution(opt.frame, 512, 7))
-    model:add(nn.Threshold())
+    model:add(nn.TemporalConvolution(opt.frame, 512, 7, 1))
+ 	model:add(nn.Threshold())
     model:add(nn.TemporalMaxPooling(2,2))
 
     -- second layer (147x512) 
-    model:add(nn.TemporalConvolution(512, 512, 7))
-    model:add(nn.Threshold())
-    model:add(nn.TemporalMaxPooling(3,3))
-
-    -- 1st fully connected layer (19x512)
-    model:add(nn.Reshape(30*512))
-    model:add(nn.Linear(30*512,1024))
+--    model:add(nn.TemporalConvolution(512, 512, 7))
+--    model:add(nn.Threshold())
+--    model:add(nn.TemporalMaxPooling(3,3))
+--
+--    -- 1st fully connected layer (19x512)
+    model:add(nn.Reshape(504*512))
+    model:add(nn.Linear(504*512,1024))
     model:add(nn.Threshold())
     model:add(nn.Dropout(0.7))
 
@@ -304,7 +313,6 @@ function main()
 
     -- randomly initialize weights  
     -- model:getParameters():uniform(-opt.init_weight, opt.init_weight)
-
 	print("\nTraining model...")
     for i=1,opt.nEpochs do
         epoch = i
@@ -314,7 +322,7 @@ function main()
             local training_data, training_labels = preprocess_train_data(n_raw_data, dictionary, opt)
     		train_model(model, criterion, training_data, training_labels, opt)
         end
-        confusion:zero()
+--        confusion:zero()
         local test_data, test_labels = preprocess_test_data(n_raw_data, dictionary, opt)
         test_model(model,test_data,test_labels,opt)
 	end
